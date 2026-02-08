d2d088dfa1c8c0e1977553f5d7c15137e80d9e16
sy-hash <50n50n.xarc@gmail.com>
2026-02-08 14:30:54 +0900

    create project

---
+++ MindEcho/MindEchoTests/MindEchoTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindEchoTests.swift
+//  MindEchoTests
+//
+//  Created by sy-hash on 2026/02/08.
+//
+
+import Testing
+@testable import MindEcho
+
+struct MindEchoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
